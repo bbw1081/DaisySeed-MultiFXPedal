f0363ba5dac589612b2bbd49ddb3a397277f54f4
bbw1081 <181002857+bbw1081@users.noreply.github.com>
2026-03-26 15:50:50 -0400

    Added footswitch -- footswitch currently switches between bypass and mute, but will activate effects in the next iteration

--- pedal.cpp
+++ pedal.cpp
@@ -4,16 +4,23 @@
 using namespace daisy;
 using namespace daisysp;
 
-DaisySeed hw;
+DaisySeed hw; //daisy seed hardware
+bool bypass = true; //bypass flag
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
-	float volume = hw.adc.Get(0) / 65535.0;
+	float volume = hw.adc.Get(0) / 65535.0; //read volume pot
 
 	for (size_t i = 0; i < size; i++)
 	{
-		out[0][i] = in[0][i] * volume;
-		out[1][i] = in[1][i]  * volume;
+		if(bypass) {
+			out[0][i] = in[0][i] * volume;
+			out[1][i] = in[1][i]  * volume;
+		} else {
+			//effect, currently mutes
+			out[0][i] = in[0][i] * 0;
+			out[1][i] = in[1][i]  * 0;
+		}
 	}
 }
 
@@ -25,10 +32,13 @@ int main(void)
 	//start hardware logging, used for debugging over serial output
 	hw.StartLog();
 
-	//initialize potentiometer adc
-	AdcChannelConfig adcConfig;
-	adcConfig.InitSingle(seed::A0); //volume potentiometer connected to pin A0
-	hw.adc.Init(&adcConfig, 1);
+	//initialize ADC
+	const int num_adc_channels = 2; //number of adc channels in use
+	AdcChannelConfig adc_config[num_adc_channels];
+	adc_config[0].InitSingle(seed::A0); //volume potentiometer connected to pin A0
+	GPIO switch_pin;
+	switch_pin.Init(seed::A1, GPIO::Mode::INPUT, GPIO::Pull::PULLUP); //initilaize footswitch pin at A1
+	hw.adc.Init(adc_config, num_adc_channels);
 	hw.adc.Start();
 
 	//initialize audio
@@ -36,6 +46,20 @@ int main(void)
 	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
 	hw.StartAudio(AudioCallback);
 
-	while(1) {}
+	bool switch_held = false; //set switch held flag to 0
+
+	while(1) {
+		//footswitch processing
+		bool switch_state = switch_pin.Read();
+		if (!switch_state && !switch_held){
+			bypass = !bypass;
+			switch_held = true;
+		} else if (switch_state){
+			switch_held = false;
+		}
+
+		//delay the system, helps act as footswitch debounce
+		System::Delay(10);
+	}
 }
 

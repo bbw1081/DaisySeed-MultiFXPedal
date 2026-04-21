f76bb3428537dd35f1d0989362e8dde3d4e15716
bbw1081 <181002857+bbw1081@users.noreply.github.com>
2026-04-21 15:06:42 -0400

    Created example presets for each effect type

--- example_presets/v_eq.h
+++ example_presets/v_eq.h
@@ -4,5 +4,5 @@ const char preset[] = R"({
     "name": "v eq",
     "effects": ["eq"],
     "params": [[1, 0.5, 0, -0.5, 0, 0, -0.5, 0, 0.5, 1]]
-    "description": "v-shaped eq"
+    "description": "v-shaped 10 band eq"
 })";
